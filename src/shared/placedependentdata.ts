5838ef8e6d883d22a5ae86a5ad570e6d4c29aee1
gamenew09 <gamenew09@gmail.com>
2020-12-01 19:07:15 -0600

    New TS module that returns data based on place ID So I can have two sets of places with differing badge & asset ids

---
+++ src/shared/placedependentdata.ts
@@ -0,0 +1,24 @@
+export interface BadgeIDsList {}
+
+export interface AssetIDsList {}
+
+export interface PlaceDependentData {
+    BadgeLookup: BadgeIDsList;
+    AssetLookup: AssetIDsList;
+}
+
+const placeId = game.PlaceId;
+
+const datas: Map<number, PlaceDependentData> = new Map<number, PlaceDependentData>();
+
+datas.set(6026623952, {
+    BadgeLookup: {
+        test: 0,
+    },
+    AssetLookup: {},
+});
+
+const data = datas.get(placeId)!;
+assert(data !== undefined, `Place ID ${placeId} does not have a valid place dependent dataset, please set one!`);
+
+export default data;
